68de9758c6d65a6ee17709a702d378727d3ae5aa
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-09-08 12:35:02 -0700

    add normalize function

--- app.py
+++ app.py
@@ -57,7 +57,13 @@ df['Count of nukechina'] = df['Text'].str.count('nukechina')
 #app layout section
 
 
-
+def normalize(df):
+    result = df.copy()
+    for feature_name in df.columns:
+        max_value = df[feature_name].max()
+        min_value = df[feature_name].min()
+        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+    return result
 
 
 
