bc4e1df81dc993bb9b92f3b544326a504d5a5643
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-09-12 13:43:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -98,13 +98,13 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     html.Br(),
     dcc.Graph(id = 'my_tweet_map', figure={}),
     dcc.Graph(id = 'my_unemployment_map', figure={}),
-    dcc.Graph(id = 'stacked_bargraph', figure={})
+    dcc.Graph(id = 'my_covid_map', figure={}),
 ])
 
 @app.callback(
      [Output(component_id='my_tweet_map', component_property='figure'),
        Output(component_id='my_unemployment_map', component_property='figure'),
-       Output(component_id='stacked_bargraphf', component_property='figure')],
+      Output(component_id='my_covid_map', component_property='figure')],
     [Input(component_id='selected_keyword', component_property='value')])
 def update_graph(option_selected):
     # df2 = pd.read_csv("unemployment.csv")
@@ -113,20 +113,11 @@ def update_graph(option_selected):
     # df2.index = df2['Datetime']
     # df2 = df2.resample('M').sum().reset_index()
     # df2['Datetime'] = pd.to_datetime(df2['Datetime'], utc = True)
-#     result = pd.merge(merged, df2, how= 'outer', on=["Datetime"])
-#     dffn = normalize(dff)
-#     dftrumpn = normalize(dftrump)
-#     df2n = normalize(df2)
-#     fig = px.line(dff, x="Datetime", y=dffn['Count of {}'.format(y)], title = "Covid Cases Increases by Date in Different States")
-#     fig.add_scatter(x=dftrump['Datetime'], y=dftrumpn['Count of {}'.format(y)])
-#     fig.add_scatter(x=df2['Datetime'], y=df2n['Unemployment_Rate'])
     dftweet = df.copy()
     dftweet['Datetime'] = pd.to_datetime(dftweet['Datetime'], errors='coerce')
     s = pd.to_datetime(dftweet['Datetime'])
     df33 = s.groupby(s.dt.floor('d')).size().reset_index(name='count')
     df33['Datetime'] = pd.to_datetime(df33['Datetime'], errors='coerce')
-
-
     dftrump = pd.read_csv("Trump Hate Tweets - Sheet1.csv")
     dftrump['Date:'] = pd.to_datetime(dftrump['Date:'], errors='coerce')
     ss = pd.to_datetime(dftrump['Date:'])
@@ -136,65 +127,56 @@ def update_graph(option_selected):
     dff['Datetime'] = pd.to_datetime(dff['Datetime'], errors='coerce')
     dff['Datetime'] = pd.to_datetime(dff['Datetime'], utc = True)
     df33['Datetime'] = pd.to_datetime(df33['Datetime'], utc = True)
-
     mergedd = df33.merge(dff, how='right', on='Datetime')
     dfcovid = pd.read_csv('covid_cases_US.csv')
-
     # merged = normalize(merged)
     dffn = normalize(df33)
     dftrumpn = normalize(mergedd)
-    df2n = normalize(df2)
+    # df2n = normalize(df2)
     tweet = pd.read_csv("ALL_TWEET_SENTIMENT.csv",parse_dates=['Datetime'])
     tweet = tweet.set_index('Datetime')
-
     result = tweet.reset_index().groupby(                                        \
               [pd.Grouper(key='Datetime', freq='1d'), 'analysis'] \
             ).count().unstack(fill_value=0).stack().reset_index()
     result=result[pd.to_numeric(result['Text'], errors='coerce').notnull()]
     resulty = result[['Datetime', 'Text', ]]
-
-    covid = results_df
+    covid = pd.read_csv('Covid_data.csv')
     covid['submission_date'] = pd.to_datetime(covid['submission_date'], errors='coerce')
-
     covid.index = covid['submission_date']
     covid = covid.resample('d').sum().reset_index()
     covid['submission_date'] = pd.to_datetime(covid['submission_date'], utc = True)
-    covid = covid[['submission_date', 'new_death']]
-    covid=covid[pd.to_numeric(covid['new_death'], errors='coerce').notnull()]
-    covid['new_death'] = covid['new_death'].astype(float)
+    covid = covid[['submission_date', 'new_case']]
+    covid=covid[pd.to_numeric(covid['new_case'], errors='coerce').notnull()]
+    covid['new_case'] = covid['new_case'].astype(float)
     covidn = normalize(covid)
     # result['Text'] = result['Text'].astype(float)
     resultn = normalize(resulty)
-
     fig = px.line(result, x= 'Datetime', y=resultn['Text'],color='analysis', title = "Covid Cases Increases by Date in Different States")
     fig.add_scatter(x=mergedd['Datetime'], y=dftrumpn['count_y'])
 #     fig.add_scatter(x=df2['Datetime'], y=df2n['Unemployment_Rate'])
 
 
     #fig= px.line(covid, x= "Date", y=covid['positiveIncrease'],title = "Tweet Mention of China Virus")
-    fig2 = px.line(x=covid['submission_date'], y=covid['new_death'])
+#     fig.add_scatter(x=covid['submission_date'], y=covidn['new_death'])
+    fig3 = px.line(covid, x='submission_date', y=covid['new_case'], title= "New Covid Cases Nationally")
 
 
 
     tweet = pd.read_csv("ALL_TWEET_SENTIMENT.csv")
     tweet = tweet[tweet['key word'].map(tweet['key word'].value_counts()) > 900]
-
     tweet['Datetime'] = pd.to_datetime(tweet['Datetime'], errors='coerce')
     tweet['Datetime'] = pd.to_datetime(tweet['Datetime'], utc = True)
     tweet = tweet[['Datetime', 'key word']]
     # tweet = tweet.set_index('Datetime')
-
     result = tweet.reset_index().groupby(                                        \
                   [pd.Grouper(key='Datetime', freq='1w'), 'key word'] \
                 ).count().unstack(fill_value=0).stack().reset_index()
     # result=result[pd.to_numeric(result['new_case'], errors='coerce').notnull()]
-    fig3 = px.bar(result, x="Datetime", y="index", color="key word", title="Count of Derogatory Slurs Used on Twitter")
-
-    # fig3 = px.line(df2, x="Datetime", y= 'Unemployment_Rate', title = "Covid Cases Increases by Date in Different States")
+    fig2 = px.bar(result, x="Datetime", y="index", color="key word", title="Count of Racial Slurs Used on Twitter")
+    # fig2 = px.line(df2, x="Datetime", y= 'Unemployment_Rate', title = "Covid Cases Increases by Date in Different States")
     fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
     fig.update_yaxes(showline=True, linewidth=2, linecolor='black')
-
-    return fig, fig2, fig3 #the return obj will be the output and if there are many output, it will go in order ( 1 obj => 1st output)
+    return fig, fig2, fig3  #the return obj will be the output and if there are many output, it will go in order ( 1 obj => 1st output)
 
 
 if __name__ == '__main__':
