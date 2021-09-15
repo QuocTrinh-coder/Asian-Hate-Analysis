341a2f87afc5296a83b581d0331ac784af7081f6
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-09-15 16:16:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -31,27 +31,13 @@ log.setLevel(logging.ERROR)
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
 server = app.server
-
 colors = {
     'background': '#FFFFFF',
-    'text': '#000000'}
+    'text': '#808080'}
 
 #data section
 df = pd.read_csv("ALL_TWEET_SENTIMENT.csv")
-key_words=["China Virus",
-'Wuhan Virus',
-'Chinacoronavirus',
-'China Corona Virus',
-'Asian Virus',
-'fuckchina',
-'Go Back to China',
-'chinaliedpeopledied',
-'Chinese Virus',
-'Bat Eater',
-'nukechina'  ]
-
-dfkeywords= pd.DataFrame(key_words,columns=['key_words'])
-
+df33= pd.read_csv("ALL_TWEET_SENTIMENT.csv")
 
 #df = df.groupby(['state', 'incident_type', 'fy_declared'])
 #df.set_index()
@@ -67,69 +53,70 @@ df['Count of Wuhan Virus'] = df['Text'].str.count('Wuhan Virus')
 df['Count of Chinese Virus'] = df['Text'].str.count('Chinese Virus')
 df['Count of Bat Eater'] = df['Text'].str.count('Bat Eater')
 df['Count of nukechina'] = df['Text'].str.count('nukechina')
-
-
 #app layout section
 
 
-def normalize(df):
-    result = df.copy()
-    for feature_name in df.columns:
-        max_value = df[feature_name].max()
-        min_value = df[feature_name].min()
-        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
-    return result
-
-app.layout = html.Div(style={'backgroundColor': colors['background']},
-children=[
-
-    html.H1("Racist Tweet Growth Rate Since Covid Hit",
-            style={'textAlign': 'center',
-                   'color': colors['text']}),
-    # All elements from the top of the page
-    html.Div([
-        html.Div([
 
 
-            dcc.Graph(
-                id='my_tweet_map',
-                figure={}
-            ),
-        ], className='six columns'),
-        html.Div([
+app.layout = html.Div(
+        html.Div(style={'backgroundColor': colors['background'], 'color': colors['text'], 'height':'100vh', 'width':'100%', 'height':'100%', 'top':'0px', 'left':'0px'},
+                 children=[
+                    html.Div(className='four columns div-user-controls',
+                             children=[
 
-            dcc.Graph(
-                id='my_unemployment_map',
-                figure={}
-            ),
-        ], className='six columns'),
-    ], className='row'),
-    # New Div for all elements in the new 'row' of the page
-    html.Div([
-        html.H1(children='Hello Dash'),
+                                 html.H1('How has covid-19, Trump, and unemployment rates impacted anti-Asian sentiment on Twitter?',style={'textAlign': 'center', 'color': 'black'}),
+        html.H4("Amidst a global pandemic, the number of reported Asian-related crimes have spiked. Derogatory terms such as “China-Virus” sponsored by former President Donald Trump have sparked xenophobia and racism towards the Asian Community. His actions have given life to new disgustingly blatantly racist slurs. In combination with fears of COVID-19 and growing unemployment around the country, China has been pinned by many for their misfortune. The Asian-American community however has been subject to racial discrimination and violence, with news surfacing all around the country, from an elderly Asian woman attacked in San Francisco, California, forced to defend herself with a wooden plank, to the deadly spa shootings in Atlanta Georgia. Violence against the Asian-American community is rising.", style={'textAlign': 'center',  'fontSize': 14}),
+        html.H4("Our mission is to bring to light the ongoing rise in Anti-Asian sentiment in society and factors relating to its growth, by studying the correlation between national COVID-19 case surges, unemployment, Trump’s use of Asian derogatory terms, and racist tweets on Twitter. ", style={'textAlign': 'center',  'fontSize': 14})  ,
+        html.H4("We stand in solidarity with the Asian-American Community.", style={'textAlign': 'center',  'fontSize': 14}),
+                                 html.Div(
+                                     className='dropdown_items',
+                                     children=[    dcc.Dropdown(id = "selected_keyword",
+                options=[
+                    {"label": x, "value": x} for x in sorted(df33['key word'].unique())],
+                 multi=False,
+                value= "China Virus"
+                 ),
+                                         dbc.Row([
+                                             dbc.Col(dcc.Graph(id="my_tweet_map"),md=12),
 
-        html.Div(children='''
-            Dash: A web application framework for Python.
-        '''),
 
-        dcc.Graph(
-            id='my_covid_map',
-            figure={}
-        ),
-    ], className='row'),
-])
+                                         ])
+                                     ],
+                                     style={'color': '#1E1E1E'})
+                                ]
+                             ),
+                    html.Div(className='eight columns div-for-charts bg-grey',
+                             children=[
+                                 dbc.Row([
+                                 dbc.Col(dcc.Graph(id="my_covid_map"),md=6),
+                                  dbc.Col(dcc.Graph(id="stack_bargraph"),md=6),
+
+                                 ])
+                             ]), dbc.Row([
+                                dbc.Col(md=3),
 
 
 
-# app.layout = html.Div(style={'backgroundColor': colors['background']}, 
-# children=[
-#
+                                 ]), dbc.Row([
+                                dbc.Col(md=3),
+
+
+
+                                 ])
+
+                                                   ])
+)
+
+
+
+# app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+
 #     html.H1("Racist Tweet Growth Rate Since Covid Hit",
 #             style={'textAlign': 'center',
 #                    'color': colors['text']}),
 #         dcc.Dropdown(id = "selected_keyword",
 #                 options=[
-#                     {"label": x, "value": x} for x in sorted(dfkeywords['key_words'].unique())],
+#                     {"label": x, "value": x} for x in sorted(df33['key word'].unique())],
 #                  multi=False,
 #                 value= "China Virus"
 #                  ),
@@ -140,13 +127,10 @@ children=[
 #     dcc.Graph(id = 'my_covid_map', figure={}),
 # ])
 
-
-
-
 @app.callback(
-     [Output(component_id='my_tweet_map', component_property='figure'),
-       Output(component_id='my_unemployment_map', component_property='figure'),
-      Output(component_id='my_covid_map', component_property='figure')],
+     Output(component_id='my_tweet_map', component_property='figure'),
+       Output(component_id='stack_bargraph', component_property='figure'),
+      Output(component_id='my_covid_map', component_property='figure'),
     [Input(component_id='selected_keyword', component_property='value')])
 def update_graph(option_selected):
     # df2 = pd.read_csv("unemployment.csv")
@@ -187,20 +171,21 @@ def update_graph(option_selected):
     covid.index = covid['submission_date']
     covid = covid.resample('d').sum().reset_index()
     covid['submission_date'] = pd.to_datetime(covid['submission_date'], utc = True)
-    covid = covid[['submission_date', 'new_case']]
-    covid=covid[pd.to_numeric(covid['new_case'], errors='coerce').notnull()]
-    covid['new_case'] = covid['new_case'].astype(float)
+    covid = covid[['submission_date', 'new_death']]
+    covid=covid[pd.to_numeric(covid['new_death'], errors='coerce').notnull()]
+    covid['new_death'] = covid['new_death'].astype(float)
     covidn = normalize(covid)
     # result['Text'] = result['Text'].astype(float)
     resultn = normalize(resulty)
-    fig = px.line(result, x= 'Datetime', y=resultn['Text'],color='analysis', title = "Trump's Tweet and Twitter Sentiment")
+    fig = px.line(result, x= 'Datetime', y=resultn['Text'],color='analysis', title = "Covid Cases Increases by Date in Different States")
     fig.add_scatter(x=mergedd['Datetime'], y=dftrumpn['count_y'])
 #     fig.add_scatter(x=df2['Datetime'], y=df2n['Unemployment_Rate'])
 
 
     #fig= px.line(covid, x= "Date", y=covid['positiveIncrease'],title = "Tweet Mention of China Virus")
 #     fig.add_scatter(x=covid['submission_date'], y=covidn['new_death'])
-    fig3 = px.line(covid, x='submission_date', y=covid['new_case'], title= "New Covid Cases Nationally")
+    fig3 = px.line(covid, x='submission_date', y=covid['new_death'], title= "New Covid Cases Nationally")
+    fig3.add_scatter(x=df33['Datetime'], y=df33['count'])
 
 
 
@@ -215,6 +200,8 @@ def update_graph(option_selected):
                 ).count().unstack(fill_value=0).stack().reset_index()
     # result=result[pd.to_numeric(result['new_case'], errors='coerce').notnull()]
     fig2 = px.bar(result, x="Datetime", y="index", color="key word", title="Count of Racial Slurs Used on Twitter")
+    fig2.add_scatter(x=mergedd['Datetime'], y=dftrumpn['count_y'], mode='markers')
+
     # fig2 = px.line(df2, x="Datetime", y= 'Unemployment_Rate', title = "Covid Cases Increases by Date in Different States")
     fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
     fig.update_yaxes(showline=True, linewidth=2, linecolor='black')
