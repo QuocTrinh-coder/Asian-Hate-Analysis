0fac10bf85e400c3bb72326d4eb862f5974c7f4d
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-09-15 16:38:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,13 @@ df['Count of Chinese Virus'] = df['Text'].str.count('Chinese Virus')
 df['Count of Bat Eater'] = df['Text'].str.count('Bat Eater')
 df['Count of nukechina'] = df['Text'].str.count('nukechina')
 #app layout section
-
+def normalize(df):
+    result = df.copy()
+    for feature_name in df.columns:
+        max_value = df[feature_name].max()
+        min_value = df[feature_name].min()
+        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+    return result
 
 
 
