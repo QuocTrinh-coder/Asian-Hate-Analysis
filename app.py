d091fd3cff9284054136580922942a37896b3def
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-10-01 14:56:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ log.setLevel(logging.ERROR)
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server,  external_stylesheets=[dbc.themes.GRID])
 server = app.server
-
 #data section
 df = pd.read_csv("ALL_TWEET_SENTIMENT.csv")
 key_words=["China Virus",
@@ -45,13 +44,8 @@ key_words=["China Virus",
 'Chinese Virus',
 'Bat Eater',
 'nukechina'  ]
-
 dfkeywords= pd.DataFrame(key_words,columns=['key_words'])
-
-
 #app layout section
-
-
 def normalize(df):
     result = df.copy()
     for feature_name in df.columns:
@@ -59,16 +53,12 @@ def normalize(df):
         min_value = df[feature_name].min()
         result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
     return result
-
-
 colors = {
     'background': '#FFFFFF',
     'text': '#808080'}
-
 #data section
 df = pd.read_csv("ALL_TWEET_SENTIMENT.csv")
 df33= pd.read_csv("ALL_TWEET_SENTIMENT.csv")
-
 #df = df.groupby(['state', 'incident_type', 'fy_declared'])
 #df.set_index()
 # print(df)
@@ -84,16 +74,11 @@ df['Count of Chinese Virus'] = df['Text'].str.count('Chinese Virus')
 df['Count of Bat Eater'] = df['Text'].str.count('Bat Eater')
 df['Count of nukechina'] = df['Text'].str.count('nukechina')
 #app layout section
-
-
-
-
 app.layout = html.Div(
         html.Div(style={'backgroundColor': colors['background'], 'color': colors['text'], 'height':'100vh', 'width':'100%', 'height':'100%', 'top':'0px', 'left':'0px'},
                  children=[
                     html.Div(className='four columns div-user-controls',
                              children=[
-
                                  html.H1('How has covid-19, Trump, and unemployment rates impacted anti-Asian sentiment on Twitter?',style={'textAlign': 'center', 'color': 'black'}),
         html.H4("Amidst a global pandemic, the number of reported Asian-related crimes have spiked. Derogatory terms such as “China-Virus” sponsored by former President Donald Trump have sparked xenophobia and racism towards the Asian Community. His actions have given life to new disgustingly blatantly racist slurs. In combination with fears of COVID-19 and growing unemployment around the country, China has been pinned by many for their misfortune. The Asian-American community however has been subject to racial discrimination and violence, with news surfacing all around the country, from an elderly Asian woman attacked in San Francisco, California, forced to defend herself with a wooden plank, to the deadly spa shootings in Atlanta Georgia. Violence against the Asian-American community is rising.", style={'textAlign': 'center',  'fontSize': 14}),
         html.H4("Our mission is to bring to light the ongoing rise in Anti-Asian sentiment in society and factors relating to its growth, by studying the correlation between national COVID-19 case surges, unemployment, Trump’s use of Asian derogatory terms, and racist tweets on Twitter. ", style={'textAlign': 'center',  'fontSize': 14})  ,
@@ -108,8 +93,6 @@ app.layout = html.Div(
                  ),
                                          dbc.Row([
                                              dbc.Col(dcc.Graph(id="my_tweet_map"),md=12),
-
-
                                          ])
                                      ],
                                      style={'color': '#1E1E1E'})
@@ -124,37 +107,21 @@ app.layout = html.Div(
                              ]), dbc.Row([
                                 dbc.Col(md=3),
                                 dbc.Col(dcc.Graph(id="stack_bargraph"),md=12)
-
-
                                  ]), dbc.Row([
                                 dbc.Col(md=3),
-
-
-
                                  ])
-
                                                    ])
 )
-
-
-
 @app.callback(
      Output(component_id='my_tweet_map', component_property='figure'),
        Output(component_id='stack_bargraph', component_property='figure'),
       Output(component_id='my_covid_map', component_property='figure'),
       Output(component_id='unemployment_graph', component_property='figure'),
     [Input(component_id='selected_keyword', component_property='value')])
-def format_title(title, subtitle=None, subtitle_font_size=14):
-    title = f'<b>{title}</b>'
-    if not subtitle:
-        return title
-    subtitle = f'<span style="font-size: {subtitle_font_size}px;">{subtitle}</span>'
-    return f'{title}<br>{subtitle}'
-
 def update_graph(option_selected):
+
     df2 = pd.read_csv("Unemployment.csv")
     df2['Datetime'] = pd.to_datetime(df2['Datetime'], errors='coerce')
-    #
     df2.index = df2['Datetime']
     df2 = df2.resample('M').sum().reset_index()
     df2['Datetime'] = pd.to_datetime(df2['Datetime'], utc = True)
@@ -174,10 +141,10 @@ def update_graph(option_selected):
     df33['Datetime'] = pd.to_datetime(df33['Datetime'], utc = True)
     mergedd = df33.merge(dff, how='right', on='Datetime')
     dfcovid = pd.read_csv('covid_cases_US.csv')
-    # merged = normalize(merged)
     dffn = normalize(df33)
     dftrumpn = normalize(mergedd)
     df2n = normalize(df2)
+
     tweet = pd.read_csv("ALL_TWEET_SENTIMENT.csv",parse_dates=['Datetime'])
     tweet = tweet.set_index('Datetime')
     result = tweet.reset_index().groupby(                                        \
@@ -185,6 +152,7 @@ def update_graph(option_selected):
             ).count().unstack(fill_value=0).stack().reset_index()
     result=result[pd.to_numeric(result['Text'], errors='coerce').notnull()]
     resulty = result[['Datetime', 'Text', ]]
+
     covid = pd.read_csv('Covid_data.csv')
     covid['submission_date'] = pd.to_datetime(covid['submission_date'], errors='coerce')
     covid.index = covid['submission_date']
@@ -194,45 +162,32 @@ def update_graph(option_selected):
     covid=covid[pd.to_numeric(covid['new_death'], errors='coerce').notnull()]
     covid['new_death'] = covid['new_death'].astype(float)
     covidn = normalize(covid)
-    # result['Text'] = result['Text'].astype(float)
     resultn = normalize(resulty)
-    custom_template = {
-    "layout": go.Layout(
-        font={
-            "family": "Nunito",
-            "size": 12,
-            "color": "#707070",
-        },
-        title={
-            "font": {
-                "family": "Lato",
-                "size": 18,
-                "color": "#1f1f1f",
-            },
-        },
-        plot_bgcolor="#ffffff",
-        paper_bgcolor="#ffffff",
-        colorway=px.colors.qualitative.G10,
-    )
-}
-    fig = px.line(result, x= 'Datetime', y=resultn['Text'],color='analysis', title = format_title("Trump and Twitter Tweets with Anti-Asian Vocabularly Over Time ","This is a graph comparing Trump’s use of Anti-Asian rhetoric on Twitter, to the overall trends of posts on Twitter that use Anti-Asian slurs. Negative Tweets refer to tweets that are hateful towards the Asian Community. Positive Tweets refer to Tweets that are in opposition to Asian hate or are in support of ending Asian hate. Neutral refers to tweets that are neither positive nor negative. Select lines in the key to make the corresponding line disappear from the graph. The data from Trump’s tweets were scraped from https://www.thetrumparchive.com/. The overall tweets were scraped from Twitter’s database using a Twitter developer account.\n *All values were normalized on the y-axis for viewing purposes*"),
-    labels = {'x':"",
-              'y':'Magnitude',},template=custom_template)
-
-
+    fig = px.line(result, x= 'Datetime',
+                  y=resultn['Text'],
+                  color='analysis', )
+    fig.update_layout(
+        title=go.layout.Title(
+            text="<b>Trump and Twitter Tweets with Anti-Asian Vocabularly Over Time</b> <br><sup>Plot Subtitle</sup>",
+            xref="paper",
+            x=0
+        ),
+            xaxis=go.layout.XAxis(
+            title=go.layout.xaxis.Title(
+                text="Datetime<br><sup>We scraped Twitter filtering for tweets that include derogatory terms against Asians. We then used an algorithm to determine the polarity of each tweet, separating them into three categories: Negative Tweets, Positive Tweets, and Neutral Tweets.<br> We included a fourth line representing the amount of time's Former President Donald Trump used a derogatory term in his Tweets. Because of Trump's permanent ban on Twitter we scraped his Tweets from the Trump Archive(https://www.thetrumparchive.com/).</sup>"
+                )
+            ),
+           yaxis=go.layout.YAxis(
+            title=go.layout.yaxis.Title(
+                text="Magnitude"
+                )
+            ),
+        )
     fig.add_scatter(x=mergedd['Datetime'], y=dftrumpn['count_y'],   name='Anti-Asian Trump Tweets',)
-    fig4 = px.line(x=df2['Datetime'], y=df2['Unemployment_Rate'], title = "Unemployment Rate",
-    labels = {'x':"This is a graph comparing the relationship between unemployment in the US and Anti-Asian tweets.\n Select the line in the key to make the corresponding line disappear from the graph.\n The National unemployment data used in the graph was gathered from https://www.bls.gov/. The Anti-Asian Tweets were scraped using a Twitter developer account from Twitter’s database.",
-              'y':'National Unemployment Rate'})
 
-
-    #fig= px.line(covid, x= "Date", y=covid['positiveIncrease'],title = "Tweet Mention of China Virus")
-#     fig.add_scatter(x=covid['submission_date'], y=covidn['new_death'])
-    fig3 = px.line(covid, x='submission_date', y=covid['new_death'], title= "New Covid Cases Nationally",
-    labels = {'x':"This is a graph comparing the rise in national COVID-19 cases and Anti-Asian tweets on Twitter.  Select the line in the key to make the corresponding line disappear from the graph. The data of national COVID-19 cases were gathered from the CDC at: https://data.cdc.gov/Case-Surveillance/United-States-COVID-19-Cases-and-Deaths-by-State-o/9mfq-cb36. The Anti-Asian Tweets were scraped from Twitter’s database using a Twitter developer account.",
-              'y':'Number of New COVID-19 Deaths'})
+    fig4 = px.line(x=df2['Datetime'], y=df2n['Unemployment_Rate'], title = "Unemployment Rate")
+    fig3 = px.line(covid, x='submission_date', y=covid['new_death'], title= "New Covid Cases Nationally")
     fig3.add_scatter(x=df33['Datetime'], y=df33['count'],   name='Anti-Asian Tweets',)
-
 
 
     tweet = pd.read_csv("ALL_TWEET_SENTIMENT.csv")
@@ -240,19 +195,13 @@ def update_graph(option_selected):
     tweet['Datetime'] = pd.to_datetime(tweet['Datetime'], errors='coerce')
     tweet['Datetime'] = pd.to_datetime(tweet['Datetime'], utc = True)
     tweet = tweet[['Datetime', 'key word']]
-    # tweet = tweet.set_index('Datetime')
     result = tweet.reset_index().groupby(                                        \
-                  [pd.Grouper(key='Datetime', freq='1w'), 'key word'] \
+                  [pd.Grouper(key='Datetime', freq='2w'), 'key word'] \
                 ).count().unstack(fill_value=0).stack().reset_index()
-    # result=result[pd.to_numeric(result['new_case'], errors='coerce').notnull()]
-    fig2 = px.bar(result, x="Datetime", y="index", color="key word", title="Count of Racial Slurs Used on Twitter",
-    labels = {'x':"This is a graph representing the number of times a specific derogatory term was used each week along side a timeline of when Trump used Anti-Asian Rhetoric on Twitter. Select any of the keywords in the key to make the corresponding bar disappear from the graph. Data of specific derogatory terms directed towards Asians were scraped from Twitter’s database. The dates of Trump’s Tweets were scraped from https://www.thetrumparchive.com/.",
-              'y':'Number of Tweets'})
+    fig2 = px.bar(result, x="Datetime", y="index", color="key word", title="Count of Racial Slurs Used on Twitter")
     fig2.add_scatter(x=mergedd['Datetime'], y=dftrumpn['count_y'], mode='markers')
-
-    # fig2 = px.line(df2, x="Datetime", y= 'Unemployment_Rate', title = "Covid Cases Increases by Date in Different States")
+    fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
+    fig.update_yaxes(showline=True, linewidth=2, linecolor='black')
     return fig, fig2, fig3, fig4  #the return obj will be the output and if there are many output, it will go in order ( 1 obj => 1st output)
-
-
 if __name__ == '__main__':
     app.run_server(debug=True,use_reloader=False, port = 9001)
